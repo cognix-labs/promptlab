6098a16845571979e4533858c85a33fb94829d4a
Raihan Alam <raihanalam@microsoft.com>
2025-05-14 01:02:10 +1000

    prog

--- samples/custom_model/custom_model.py
+++ samples/custom_model/custom_model.py
@@ -8,24 +8,25 @@ tracer_config = {"type": "sqlite", "db_file": "./promptlab.db"}
 pl = PromptLab(tracer_config)
 
 # Create a prompt template
-prompt_template = PromptTemplate(
-    name="essay_feedback",
-    description="A prompt for generating feedback on essays",
-    system_prompt="You are a helpful assistant who can provide feedback on essays.",
-    user_prompt="""The essay topic is - <essay_topic>.
-        The submitted essay is - <essay>
-        Now write feedback on this essay.
-        """,
-)
-pt = pl.asset.create(prompt_template)
+prompt_name = "essay_feedback"
+prompt_description = "A prompt for generating feedback on essays"
+system_prompt = "You are a helpful assistant who can provide feedback on essays."
+user_prompt = """The essay topic is - <essay_topic>.
+               The submitted essay is - <essay>
+               Now write feedback on this essay."""
+prompt_template = PromptTemplate(name=prompt_name, description=prompt_description, system_prompt=system_prompt, user_prompt=user_prompt)
+# pt = pl.asset.create(prompt_template)
 
 # Create a dataset
-dataset = Dataset(
-    name="essay_samples",
-    description="dataset for evaluating the essay_feedback prompt",
-    file_path="./samples/data/essay_feedback.jsonl",
-)
-ds = pl.asset.create(dataset)
+dataset_name = "essay_samples"
+dataset_description = "dataset for evaluating the essay_feedback prompt"
+dataset_file_path = "./samples/data/essay_feedback.jsonl"
+dataset = Dataset(name=dataset_name, description=dataset_description, file_path=dataset_file_path)
+# ds = pl.asset.create(dataset)
+
+# Retrieve assets
+pt = pl.asset.get(asset_name = prompt_name, version=0)
+ds = pl.asset.get(asset_name = dataset_name, version=0)
 
 # Custom model
 inference_model = {
